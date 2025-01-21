0a142adb4ac9a616af47fb2ec7711f547ec788e4
Daniel Lacasse <daniel@nokee.dev>
2025-01-21 16:32:43 -0500

    Introduce first pass of transactional compilation
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

--- src/main/java/dev/nokee/legacy/CppCompileTask.java
+++ src/main/java/dev/nokee/legacy/CppCompileTask.java
@@ -1,5 +1,6 @@
 package dev.nokee.legacy;
 
+import org.apache.commons.io.FileUtils;
 import org.gradle.api.Action;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
@@ -14,6 +15,7 @@ import org.gradle.internal.Cast;
 import org.gradle.internal.Factory;
 import org.gradle.internal.operations.logging.BuildOperationLogger;
 import org.gradle.language.base.internal.compile.Compiler;
+import org.gradle.language.base.internal.compile.VersionAwareCompiler;
 import org.gradle.language.cpp.CppBinary;
 import org.gradle.language.cpp.CppComponent;
 import org.gradle.language.cpp.plugins.CppBasePlugin;
@@ -24,6 +26,7 @@ import org.gradle.nativeplatform.internal.BuildOperationLoggingCompilerDecorator
 import org.gradle.nativeplatform.platform.internal.NativePlatformInternal;
 import org.gradle.nativeplatform.toolchain.internal.NativeCompileSpec;
 import org.gradle.nativeplatform.toolchain.internal.NativeToolChainInternal;
+import org.gradle.nativeplatform.toolchain.internal.OutputCleaningCompiler;
 import org.gradle.nativeplatform.toolchain.internal.PlatformToolProvider;
 import org.gradle.util.GradleVersion;
 import org.gradle.work.InputChanges;
@@ -31,7 +34,10 @@ import org.gradle.work.InputChanges;
 import javax.annotation.Nullable;
 import javax.inject.Inject;
 import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.*;
 import java.util.concurrent.Callable;
@@ -72,7 +78,8 @@ public abstract /*final*/ class CppCompileTask extends CppCompile {
 		Class<T> specType = Cast.uncheckedCast(spec.getClass());
 		Compiler<T> baseCompiler = platformToolProvider.newCompiler(specType);
 		Compiler<T> perSourceCompiler = newPerSourceCompiler(baseCompiler, perSourceOptions::forFile, () -> (T) createCompileSpec());
-		Compiler<T> incrementalCompiler = getIncrementalCompiler().createCompiler(perSourceCompiler);
+		Compiler<T> transactionalCompiler = newTransactionalCompiler(perSourceCompiler, outputFileDir(baseCompiler));
+		Compiler<T> incrementalCompiler = getIncrementalCompiler().createCompiler(transactionalCompiler);
 		Compiler<T> loggingCompiler = BuildOperationLoggingCompilerDecorator.wrap(incrementalCompiler);
 		return loggingCompiler.execute(spec);
 	}
@@ -86,6 +93,133 @@ public abstract /*final*/ class CppCompileTask extends CppCompile {
 		} catch (NoSuchFieldException | IllegalAccessException e) {
 			throw new RuntimeException(e);
 		}
+	}
+
+	private <T extends NativeCompileSpec> Compiler<T> newTransactionalCompiler(Compiler<T> delegateCompiler, OutputFileDirResolver outputFileDirResolver) {
+		return new Compiler<T>() {
+			@Override
+			public WorkResult execute(T spec) {
+				File temporaryDirectory = new File(spec.getTempDir(), "compile-transaction");
+				File stashDirectory = new File(temporaryDirectory, "stash");
+				File backupDirectory = new File(temporaryDirectory, "backup");
+
+				List<StashedFile> stashedFiles = stashFiles(spec.getRemovedSourceFiles(), spec.getObjectFileDir(), stashDirectory);
+				List<StashedFile> backupFiles = stashFiles(spec.getSourceFiles(), spec.getObjectFileDir(), backupDirectory);
+
+				// TODO: We should coerce the per-source options to ensure logging happens (start and done at the end)
+				BuildOperationLogger delegate = spec.getOperationLogger();
+				spec.setOperationLogger(new BuildOperationLogger() {
+					private boolean failed = false;
+
+					@Override
+					public void start() {
+						delegate.start();
+					}
+
+					@Override
+					public void operationSuccess(String description, String output) {
+						delegate.operationSuccess(description, output);
+					}
+
+					@Override
+					public void operationFailed(String description, String output) {
+						failed = true;
+						delegate.operationFailed(description, output);
+					}
+
+					@Override
+					public void done() {
+						if (failed) {
+							stashedFiles.forEach(StashedFile::unstash);
+							backupFiles.forEach(StashedFile::unstash);
+						} else {
+							try {
+								FileUtils.deleteDirectory(temporaryDirectory);
+							} catch (IOException e) {
+								throw new RuntimeException(e);
+							}
+						}
+						delegate.done();
+					}
+
+					@Override
+					public String getLogLocation() {
+						return delegate.getLogLocation();
+					}
+				});
+
+				// capture CommandLineToolInvocationFailure with message "C++ compiler failed while compiling " suffix
+				return delegateCompiler.execute(spec);
+			}
+
+			List<StashedFile> stashFiles(List<File> filesToStash, File objectFileDir, File stashDirectory) {
+				return filesToStash.stream().map(file -> {
+					File origFile = outputFileDirResolver.outputFileDir(file, objectFileDir).getParentFile();
+					File stashedFile = outputFileDirResolver.outputFileDir(file, stashDirectory).getParentFile();
+
+					if (origFile.exists()) {
+						try {
+							FileUtils.copyDirectory(origFile, stashedFile, true);
+						} catch (IOException e) {
+							throw new RuntimeException(e);
+						}
+
+						return new StashedFile() {
+							@Override
+							public void unstash() {
+								try {
+									FileUtils.deleteDirectory(origFile);
+									FileUtils.copyDirectory(stashedFile, origFile, true);
+								} catch (IOException e) {
+									throw new RuntimeException(e);
+								}
+							}
+						};
+					} else {
+						return new StashedFile() {
+							@Override
+							public void unstash() {
+								try {
+									FileUtils.deleteDirectory(origFile);
+								} catch (IOException e) {
+									throw new RuntimeException(e);
+								}
+							}
+						};
+					}
+				}).collect(Collectors.toList());
+			}
+
+			private abstract class StashedFile {
+				public abstract void unstash();
+			}
+		};
+	}
+
+	private OutputFileDirResolver outputFileDir(Compiler<?> nativeCompiler) {
+		try {
+			Field VersionAwareCompiler_compiler = VersionAwareCompiler.class.getDeclaredField("compiler");
+			VersionAwareCompiler_compiler.setAccessible(true);
+			nativeCompiler = (Compiler<?>) VersionAwareCompiler_compiler.get(nativeCompiler);
+
+			Method OutputCleaningCompiler_getObjectFile = OutputCleaningCompiler.class.getDeclaredMethod("getObjectFile", File.class, File.class);
+			OutputCleaningCompiler_getObjectFile.setAccessible(true);
+
+			final Compiler<?> self = nativeCompiler;
+			return (sourceFile, objectFileDir) -> {
+				try {
+					return (File) OutputCleaningCompiler_getObjectFile.invoke(self, objectFileDir, sourceFile);
+				} catch (IllegalAccessException | InvocationTargetException e) {
+					throw new RuntimeException(e);
+				}
+			};
+		} catch (NoSuchFieldException | IllegalAccessException | NoSuchMethodException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	interface OutputFileDirResolver {
+		File outputFileDir(File sourceFile, File objectFileDir);
 	}
 
 	@Inject
