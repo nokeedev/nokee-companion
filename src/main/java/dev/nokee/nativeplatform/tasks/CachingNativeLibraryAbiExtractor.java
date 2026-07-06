0eaa58b6a590cebbec365019946bc3cb489df315
Daniel Lacasse <daniel@nokee.dev>
2026-07-06 11:45:58 -0400

    Use file size and modtime instead of file hash
    Parsing the symbol table implies a certain file hashing (aka reading a
    big chunk of the file) so we incur a performance hit if we hash and,
    optionally, parse the file.
    
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

--- src/main/java/dev/nokee/nativeplatform/tasks/CachingNativeLibraryAbiExtractor.java
+++ src/main/java/dev/nokee/nativeplatform/tasks/CachingNativeLibraryAbiExtractor.java
@@ -1,27 +1,22 @@
 package dev.nokee.nativeplatform.tasks;
 
 import org.gradle.api.model.ObjectFactory;
-import org.gradle.internal.hash.HashCode;
-import org.gradle.internal.vfs.FileSystemAccess;
 
 import javax.annotation.Nullable;
 import javax.inject.Inject;
 import java.nio.file.Path;
 
 abstract /*final*/ class CachingNativeLibraryAbiExtractor implements NativeLibraryAbiExtractor {
-	private final FileSystemAccess fileAccess;
 	private final LinkAbiCache cache;
 	private final DefaultNativeLibraryAbiExtractor extractor;
 
 	@Inject
-	public CachingNativeLibraryAbiExtractor(FileSystemAccess fileAccess, LinkAbiCache cache, ObjectFactory objects) {
-		this.fileAccess = fileAccess;
+	public CachingNativeLibraryAbiExtractor(LinkAbiCache cache, ObjectFactory objects) {
 		this.cache = cache;
 		this.extractor = new DefaultNativeLibraryAbiExtractor(objects);
 	}
 
 	public @Nullable AbiEntry extract(Path library) {
-		HashCode hash = fileAccess.readRegularFileContentHash(library.toString()).orElseThrow(RuntimeException::new);
-		return cache.find(hash.toString(), () -> extractor.extract(library));
+		return cache.find(library, () -> extractor.extract(library));
 	}
 }
--- src/main/java/dev/nokee/nativeplatform/tasks/LinkAbiCache.java
+++ src/main/java/dev/nokee/nativeplatform/tasks/LinkAbiCache.java
@@ -4,23 +4,50 @@ import org.gradle.api.services.BuildService;
 import org.gradle.api.services.BuildServiceParameters;
 
 import javax.inject.Inject;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributeView;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
 
 abstract /*final*/ class LinkAbiCache implements BuildService<BuildServiceParameters.None> {
-	private final Map<String, AbiEntry> cache = new ConcurrentHashMap<>();
+	private final Map<File, CachedAbiEntry> cache = new ConcurrentHashMap<>();
 
 	@Inject
 	public LinkAbiCache() {}
 
-	public AbiEntry find(String hash, Callable<AbiEntry> mapper) {
-		return cache.computeIfAbsent(hash, __ -> {
+	public AbiEntry find(Path path, Callable<AbiEntry> mapper) {
+		return cache.compute(path.toFile(), (File f, CachedAbiEntry value) -> {
 			try {
-				return mapper.call();
+				BasicFileAttributes attributes = Files.getFileAttributeView(f.toPath(), BasicFileAttributeView.class).readAttributes();
+
+				if (value == null) {
+					return new CachedAbiEntry(attributes.size(), attributes.lastModifiedTime().toMillis(), mapper.call());
+				}
+
+				if (attributes.size() == value.size && attributes.lastModifiedTime().toMillis() == value.modtime) {
+					return value;
+				}
+
+				return new CachedAbiEntry(attributes.size(), attributes.lastModifiedTime().toMillis(), mapper.call());
 			} catch (Exception e) {
 				throw new RuntimeException(e);
 			}
-		});
+		}).entry;
+	}
+
+	private static final class CachedAbiEntry {
+		private final long size;
+		private final long modtime;
+		private final AbiEntry entry;
+
+		public CachedAbiEntry(long size, long modtime, AbiEntry entry) {
+			this.size = size;
+			this.modtime = modtime;
+			this.entry = entry;
+		}
 	}
 }
