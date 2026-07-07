2a5f7645708e5b883d898b8e9af014dda6173c6a
Daniel Lacasse <daniel@nokee.dev>
2026-07-07 12:38:02 -0400

    Use libs from the link ABI extension
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

--- src/main/java/dev/nokee/nativeplatform/tasks/LinkAbiAware.java
+++ src/main/java/dev/nokee/nativeplatform/tasks/LinkAbiAware.java
@@ -2,25 +2,21 @@ package dev.nokee.nativeplatform.tasks;
 
 import org.gradle.api.Task;
 import org.gradle.api.file.ConfigurableFileCollection;
-import org.gradle.api.file.FileCollection;
 import org.gradle.api.file.FileSystemLocation;
 import org.gradle.api.file.ProjectLayout;
 import org.gradle.api.model.ObjectFactory;
 import org.gradle.api.provider.MapProperty;
 import org.gradle.api.provider.Property;
-import org.gradle.api.provider.Provider;
 import org.gradle.api.provider.SetProperty;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.InputFiles;
 import org.gradle.api.tasks.Internal;
 import org.gradle.api.tasks.Nested;
-import org.gradle.nativeplatform.tasks.AbstractLinkTask;
 
 import javax.inject.Inject;
 import java.io.File;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -33,10 +29,8 @@ interface LinkAbiAware extends Task {
 	@Nested
 	default LinkAbiExtension getLinkAbi() {
 		if (!getExt_linkAbi().isPresent()) { // safe as we control the lifecycle
-			assert this instanceof AbstractLinkTask : "task must be an AbstractLinkTask";
-			FileCollection libs = ((AbstractLinkTask) this).getLibs();
 			ObjectFactory objects = getProject().getObjects();
-			LinkAbiExtension extension = objects.newInstance(LinkAbiExtension.class, libs);
+			LinkAbiExtension extension = objects.newInstance(LinkAbiExtension.class);
 			extension.getExtractor().set(getProject().getGradle().getSharedServices().registerIfAbsent("link-abi-cache", LinkAbiCache.class).map(it -> objects.newInstance(CachingNativeLibraryAbiExtractor.class, it)));
 			getExt_linkAbi().set(extension);
 		}
@@ -50,7 +44,7 @@ interface LinkAbiAware extends Task {
 		protected abstract Property<CachingNativeLibraryAbiExtractor> getExtractor();
 
 		@Inject
-		public LinkAbiExtension(FileCollection libs) {
+		public LinkAbiExtension() {
 			getFis().from(getLinkLibInputs().map(it -> {
 				return it.stream().flatMap(t -> {
 					if (t instanceof File) {
@@ -69,7 +63,7 @@ interface LinkAbiAware extends Task {
 			}));
 			getIns().disallowChanges();
 			getIns().finalizeValueOnRead();
-			getLinkLibInputs().set(libs.getElements().map(libsx -> {
+			getLinkLibInputs().set(getLibs().getElements().map(libsx -> {
 				Path root = getLayout().getProjectDirectory().getAsFile().toPath();
 				AbiExtractorService extractor = getAbiExtractor();
 				List<Object> result = new ArrayList<>();
@@ -82,6 +76,9 @@ interface LinkAbiAware extends Task {
 			getLinkLibInputs().finalizeValueOnRead(); // ensure one resolution per snapshot
 			getLinkLibInputs().disallowChanges();
 		}
+
+		@Internal
+		public abstract ConfigurableFileCollection getLibs();
 
 		@Inject protected abstract ProjectLayout getLayout();
 		@Inject protected abstract ObjectFactory getObjects();
--- src/main/java/dev/nokee/nativeplatform/tasks/LinkSharedLibraryTask.java
+++ src/main/java/dev/nokee/nativeplatform/tasks/LinkSharedLibraryTask.java
@@ -1,17 +1,36 @@
 package dev.nokee.nativeplatform.tasks;
 
 import org.gradle.api.file.ConfigurableFileCollection;
+import org.gradle.api.file.FileCollection;
 import org.gradle.api.tasks.Internal;
 
 import javax.inject.Inject;
 
 /*private*/ abstract /*final*/ class LinkSharedLibraryTask extends org.gradle.nativeplatform.tasks.LinkSharedLibrary implements LinkAbiAware {
 	@Inject
-	public LinkSharedLibraryTask() {}
+	public LinkSharedLibraryTask() {
+		linkSuperClassLibsField();
+	}
+
+	//region libs override
+	private void linkSuperClassLibsField() {
+		super.getLibs().from(getLibs());
+	}
 
 	@Override
 	@Internal
 	public ConfigurableFileCollection getLibs() {
-		return super.getLibs();
+		return getLinkAbi().getLibs();
 	}
+
+	@Override
+	public void setLibs(FileCollection libs) {
+		getLibs().setFrom(libs);
+	}
+
+	@Override
+	public void lib(Object libs) {
+		getLibs().from(libs);
+	}
+	//endregion
 }
